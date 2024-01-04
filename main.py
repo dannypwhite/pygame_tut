a1345ada268af00119ec28f3cf9f4007d07b5147
dannypwhite <dannypwhite@gmail.com>
2024-01-04 15:09:08 -0500

    Importing and initializing pygame

---
+++ main.py
@@ -0,0 +1,17 @@
+#Import pygame module
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+#Initialize pygame
+pygame.init()
