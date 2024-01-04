3268eb07d1ef793bf2d538689ba012ec0ad1b63d
dannypwhite <dannypwhite@gmail.com>
2024-01-04 15:29:20 -0500

    Setting up the screen and game loop

--- main.py
+++ main.py
@@ -15,3 +15,28 @@ from pygame.locals import (
 
 #Initialize pygame
 pygame.init()
+
+# Define constatnts for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Create the screen object
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Variable to keep main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so stop the loop.
+        elif event.type == QUIT:
+            running = False
