bdfc89c5b4e40078d02b01c9f273ec5a78f25b92
dannypwhite <dannypwhite@gmail.com>
2024-01-06 13:26:22 -0500

    added sound effects and background music

--- main.py
+++ main.py
@@ -33,8 +33,10 @@ class Player(pygame.sprite.Sprite):
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
+            move_up_sound.play()
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0, 5)
+            move_down_sound.play()
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5, 0)
         if pressed_keys[K_RIGHT]:
@@ -92,6 +94,10 @@ class Cloud(pygame.sprite.Sprite):
         self.rect.move_ip(-5, 0)
         if self.rect.right < 0:
             self.kill()
+
+# Setup for sounds.  Defaults are good
+pygame.mixer.init() 
+
 #Initialize pygame
 pygame.init()
 
@@ -123,6 +129,19 @@ all_sprites.add(player)
 running = True
 
 # Main loop
+
+# Load and play background music
+# Sound source: http://ccmixter.org/files/Apoxode/59262
+# License: https://creativecommons.org/licenses/by/3.0/
+pygame.mixer.music.load("assets/Apoxode_-_Electric_1.mp3")
+pygame.mixer.music.play(loops=-1)
+
+# Load all sound files
+# Sound source: Jon Fincher
+move_up_sound = pygame.mixer.Sound("assets/Rising_putter.ogg")
+move_down_sound = pygame.mixer.Sound("assets/Falling_putter.ogg")
+collision_sound = pygame.mixer.Sound("assets/Collision.ogg")
+
 while running:
     # Look at every event in the queue
     for event in pygame.event.get():
@@ -170,9 +189,20 @@ while running:
     if pygame.sprite.spritecollideany(player, enemies):
         # If so, then remove player and stop loop
         player.kill()
+
+        # Stop any moving sounds and play the collision sound
+        move_down_sound.stop()
+        move_up_sound.stop()
+        collision_sound.play()
+        
+        # Stop the loop
         running = False
     
     pygame.display.flip()
 
     # Ensure the program maintains a rate of 30 frames per second
     clock.tick(30)
+
+# All done! Stop and quit the mixer.
+pygame.mixer.music.stop()
+pygame.mixer.quit()
